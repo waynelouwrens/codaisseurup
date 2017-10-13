2a40d35f29794b411c59a41a6977503148e572c8
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-13 13:00:40 +0200

    One-to-Many: Controllers

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:show]
+
+  def index
+    @events = current_user.events
+  end
+
+  def show
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @events = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: "Event successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:name, :description, :location, :includes_food, :includes_drinks, :price, :starts_at, :ends_at, :capacity, :active)
+    end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,7 @@ class Event < ApplicationRecord
 
   validates :name, presence:true
   validates :description, presence:true, length: {maximum: 500 }
+  validates :starts_at, presence:true
+  validates :ends_at, presence:true,
 
 end
