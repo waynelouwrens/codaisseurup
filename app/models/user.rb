150a6d7621ae0ad08d6e46eb76afb94aff376c3b
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-16 16:02:50 +0200

    One-to-One Association User to Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :events, dependent: :destroy
+  has_one :profile
+
+  def has_profile?
+    profile.present? && profile.persisted?
+  end
+
+  def full_name
+    profile.full_name
+  end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :categories
+  resources :users, only: [:show]
+  resources :events, except: [:destroy]
+  resources :profiles, only: [:new, :edit, :create, :update]
+
   root to: 'pages#home'
 
   devise_for :users
 
-  resources :users, only: [:show]
-  resources :events, except: [:destroy]
+
 end
